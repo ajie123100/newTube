14fa0872917caa240ffcf503fefe239c376a1c98
ajie123100 <2946478276@qq.com>
2025-03-13 23:44:46 +0800

    1233445

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,33 +1,66 @@
+import { relations } from 'drizzle-orm'
 import {
   pgTable,
   uuid,
   text,
   timestamp,
   uniqueIndex,
-} from "drizzle-orm/pg-core";
+} from 'drizzle-orm/pg-core'
 
 export const users = pgTable(
-  "users",
+  'users',
   {
-    id: uuid("id").primaryKey().defaultRandom(),
-    clerkId: text("clerk_id").notNull().unique(),
-    name: text("name").notNull(),
+    id: uuid('id').primaryKey().defaultRandom(),
+    clerkId: text('clerk_id').notNull().unique(),
+    name: text('name').notNull(),
     // todo: add Banner fields
-    imageUrl: text("image_url").notNull(),
-    createdAt: timestamp("created_at").defaultNow().notNull(),
-    updatedAt: timestamp("updated_at").defaultNow().notNull(),
+    imageUrl: text('image_url').notNull(),
+    createdAt: timestamp('created_at').defaultNow().notNull(),
+    updatedAt: timestamp('updated_at').defaultNow().notNull(),
   },
-  (t) => [uniqueIndex("clerk_id_idx").on(t.clerkId)]
-);
+  (t) => [uniqueIndex('clerk_id_idx').on(t.clerkId)],
+)
+
+export const userRelations = relations(users, ({ many }) => ({
+  videos: many(videos),
+}))
 
 export const categories = pgTable(
-  "categories",
+  'categories',
   {
-    id: uuid("id").primaryKey().defaultRandom(),
-    name: text("name").notNull().unique(),
-    description: text("description"),
-    createdAt: timestamp("created_at").defaultNow().notNull(),
-    updatedAt: timestamp("updated_at").defaultNow().notNull(),
+    id: uuid('id').primaryKey().defaultRandom(),
+    name: text('name').notNull().unique(),
+    description: text('description'),
+    createdAt: timestamp('created_at').defaultNow().notNull(),
+    updatedAt: timestamp('updated_at').defaultNow().notNull(),
   },
-  (t) => [uniqueIndex("name_idx").on(t.name)]
-);
+  (t) => [uniqueIndex('name_idx').on(t.name)],
+)
+
+export const categoriesRelations = relations(categories, ({ many }) => ({
+  videos: many(videos),
+}))
+
+export const videos = pgTable('videos', {
+  id: uuid('id').primaryKey().defaultRandom(),
+  title: text('title').notNull(),
+  description: text('description'),
+  userId: uuid('user_id')
+    .notNull()
+    .references(() => users.id, {
+      onDelete: 'cascade', // 删除用户时，删除视频
+    }), // references 外键引用
+  categoryId: uuid('category_id').references(() => categories.id, {
+    onDelete: 'set null', // 删除分类时，不删除视频
+  }),
+  createdAt: timestamp('created_at').defaultNow().notNull(),
+  updatedAt: timestamp('updated_at').defaultNow().notNull(),
+})
+
+export const videoRelations = relations(videos, ({ one }) => ({
+  user: one(users, { fields: [videos.userId], references: [users.id] }),
+  category: one(categories, {
+    fields: [videos.categoryId],
+    references: [categories.id],
+  }),
+}))
